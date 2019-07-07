64f2ca0464c79468fdc79a9abd4cf3adade8aee6
Thomas Viles <thomas.c.viles.jr@gmail.com>
2019-07-07 15:52:19 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,20 @@
 class School
-  attr_reader :name
+  attr_reader :name, :roster
 
   def initialize(name)
     @name = name
+    @roster = Hash.new
+  end
+
+  def add_student(student, grade)
+    @roster[grade] << student
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.keys.sort
   end
 end
